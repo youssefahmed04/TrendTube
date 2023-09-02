9714f488712ced3e9467c0e4028af18822bed17b
youssefahmed04 <youssef.ahmed2004@gmail.com>
2023-09-02 08:12:10 -0400

    Adjusted dark-mode video text color

--- TrendTube/dist/script.75da7f30.js
+++ TrendTube/dist/script.75da7f30.js
@@ -1280,7 +1280,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62214" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54355" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
