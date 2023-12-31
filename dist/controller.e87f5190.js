dba54cd88399755c213905b00d0e11f5937b3a18
Youssef Ahmed <youssef.ahmed2004@gmail.com>
2023-12-31 14:02:57 -0500

    Fixed css for pagination buttons

--- dist/controller.e87f5190.js
+++ dist/controller.e87f5190.js
@@ -771,7 +771,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60360" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58548" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
